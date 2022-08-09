5e4527148c4cbc4bc68ca3d8d95dd4d4368e7610
blazeout <1072830776@qq.com>
2022-08-09 23:44:38 +0800

    informer practice

--- 02_Informer/main.go
+++ 02_Informer/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
 )
 
 func main() {
@@ -23,16 +25,31 @@ func main() {
 	// 如何指定具体的namespace
 	factory := informers.NewSharedInformerFactoryWithOptions(clientSet, 0, informers.WithNamespace("default"))
 	informer := factory.Core().V1().Pods().Informer()
+	rateLimitingQueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 	// 4. 注册对应事件的处理函数
 	funcs := cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			// do something
+			fmt.Println("Add Event")
+			// 将 obj 的 key 传入 queue中
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err != nil {
+				print(err)
+			}
+			rateLimitingQueue.AddRateLimited(key)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			// do something
+			fmt.Print("Update Event")
+			// 将 new obj 的 key 传入队列中
+			key, _ := cache.MetaNamespaceKeyFunc(newObj)
+			rateLimitingQueue.AddRateLimited(key)
 		},
 		DeleteFunc: func(obj interface{}) {
 			// do something
+			fmt.Println("Delete Event")
+			key, _ := cache.MetaNamespaceKeyFunc(obj)
+			rateLimitingQueue.AddRateLimited(key)
 		},
 	}
 	informer.AddEventHandler(funcs)
