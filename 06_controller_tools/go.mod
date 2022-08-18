0439a211aa1a22cf84918a1e57ae92d2324c1a0d
blazeout <1072830776@qq.com>
2022-08-18 11:11:57 +0800

    controller-tools

---
+++ 06_controller_tools/go.mod
@@ -0,0 +1,5 @@
+module wangjiahao.com
+
+go 1.18
+
+require k8s.io/client-go v0.24.1 // indirect
---
+++ 06_controller_tools/main.go
@@ -0,0 +1,6 @@
+package main
+
+// 使用controller-tools自动生成types.go文件 和 deepcopy.go文件 和 crd.yaml文件
+func main() {
+
+}
